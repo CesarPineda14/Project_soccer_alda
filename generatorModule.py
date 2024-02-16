fd3bda2b089db474ea8840d7ce24e6e0b5df147c
CesarPineda14 <cesar.pineda-d@mail.escuelaing.edu.co>
2024-02-16 00:41:08 -0500

    edad, id, apellido, nombre y pais

--- generatorModule.py
+++ generatorModule.py
@@ -1,9 +1,29 @@
 import random as rn
+from faker import Faker
 
 # funcion que retorna un numero aleatorio entre 10000000-99999999
 def randomId():
     return rn.randint(10000000, 99999999)
 
+# funcion que retorna un nombre aleatorio mediante el uo de la libreria Faker
+# type("name"): nombre random
+# type("LastName"): apellido random
+# type("country"): pais random
+def randomFaker(type):
+    faker = Faker()
+    if type=="name":
+        return faker.first_name_male()
+    elif type=="lastName" :
+        return faker.last_name()
+    elif type=="country":
+        return faker.country()
+    
+
+def randomAge():
+    ages = [17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39]
+    weight = [4,4,5,9,9,9,9,9,6,6,5,5,4,3,2,2,2,2,2,1,1,1,1]
+    return rn.choices(ages, weights=weight)
+    
 
 
 
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import generatorModule as gen
+import pandas as pd
 
-print(gen.randomId())
+
+dataDic = [
+    
+]
+
+
+for x in range(7):
+    dataDic.append( {
+                    "ID":gen.randomId(),
+                    'Name': gen.randomFaker("name"),
+                    'LastName':gen.randomFaker("lastName"),
+                    'Country':gen.randomFaker("country"),
+                    'Age':gen.randomAge()[0],
+                    })
+
+
+df = pd.DataFrame(dataDic)
+
+print(df)
